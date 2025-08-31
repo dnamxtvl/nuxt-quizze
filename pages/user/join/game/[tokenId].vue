3d7345e7be1699c1ba98d94ad10b3effd37c21e3
dnamxtvl <kakitani2000@gmail.com>
2025-09-01 01:55:03 +0700

    feat: change css

--- pages/user/join/game/[tokenId].vue
+++ pages/user/join/game/[tokenId].vue
@@ -23,23 +23,6 @@
             </div>
         </el-dialog>
         <div class="w-full d-flex show-question-body flex-column" v-if="showQuestion && currentRoomStatus > 0">
-            <!-- <div v-if="showQuestion && currentRoomStatus > 0"
-                class="row question-title d-flex flex-wrap justify-content-center g-0" style="flex:1"> -->
-                <!-- <div class="d-flex justify-content-center question-title-html">
-                    <span class="d-flex align-items-center text-dark text-center fs-2 pe-4 py-4 fw-bold lh-base">
-                        {{ currentQuestionIndex + 1 }}.
-                    </span>
-                    <!-- <div class="d-grid align-self-center lh-1 text-black question-html pe-4 fs-1 pt-2 text-start font-bold" v-html="currentQuestion?.title"></div> -->
-                <!-- </div> -->
-                <!-- <div class="container mt-2 d-flex justify-content-center" v-if="currentQuestion.image">
-                    <div class="upload-container">
-                        <img :src="currentQuestion.image" alt="Image Preview">
-                        <label class="cursor-pointer">
-                        </label>
-                    </div>
-                </div> -->
-                <!-- <h3 class="text-warning text-center fs-1">{{ timeReply }}</h3> -->
-            <!-- </div> -->
             <div v-if="showQuestion && currentRoomStatus > 0"
                 class="row list-answer  align-items-center me-0 mt-4 px-3">
                 <div class="col-12 col-sm-6 col-md-6 col-lg-6 cursor-pointer pe-0 mb-2 "
